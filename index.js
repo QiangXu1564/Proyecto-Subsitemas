c7b63cc47c99ead0e903cf0c2bef4792c405e8dd
Qiang <Qiangxu1564@gmail.com>
2024-05-13 15:32:20 +0200

    Commit 6

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const dgram = require('node:dgram');
+const writeFile = require('jsonfile');
+
+const client = dgram.createSocket('udp4');
 
 app.set('port', process.env.PORT || 8080);
 app.use(express.static(path.join(__dirname, 'html')));
 
+let serverdate;
 const server = app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
+    serverdate = new Date();
+    console.log('Fecha en el que se inicia el servidor:', serverdate)
 });
 
 const socketio = require('socket.io');
@@ -15,22 +22,40 @@ const io = socketio(server);
 let arraydatos = new Array();
 let n = 0;
 
+//Conexión con el cliente websocket
 io.on('connection', (socket) => {
     console.log('New connection', socket.id);
     for(let i = 0; i < n; i++){
         socket.emit('dataclient', arraydatos[i]);
     }
-    socket.on('data', (data) =>{
-        arraydatos[n] = data;
-        n++;
-        io.sockets.emit('dataclient', data);
-    })
-    socket.on('pruebaenvio', (data) => {
-        arraydatos[n] = data;
-        n++;
-        io.sockets.emit('dataclient', data);
-    })
-    socket.on('error', (data) => {
-        console.log("El dato: " + data + "no es un dato válido.")
-    })
+    socket.emit('serverdate', serverdate.getTime());
 })
+
+//Conexión con el servidor udp
+client.on('message', (msg, rinfo) => {
+    console.log(`Datos recibidos desde ${rinfo.address}:${rinfo.port}`);
+    const data = JSON.parse(msg);
+    let time = Math.floor(Math.abs(serverdate.getTime() - data.timestamp) / 1000);
+    data.timestamp = time;
+    console.log('Mensaje recibido del servidor:', data);
+    io.emit('dataclient', data);
+    arraydatos[n] = data;
+    n++;
+});
+  
+  
+const PORT = 3000;
+const SERVER_ADDRESS = 'localhost';
+client.bind(PORT, SERVER_ADDRESS, () => {
+    console.log(`Cliente UDP conectado y escuchando en el puerto ${PORT}`);
+});
+
+function guardarDatos(arrayDatos, nombreArchivo) {
+    writeFile.writeFile(nombreArchivo, arrayDatos, { spaces: 2 });
+}
+
+const nombreArchivo = 'datos.json';
+process.on('SIGINT', () => {
+    guardarDatos(arraydatos, nombreArchivo);
+    process.exit(0);
+});
